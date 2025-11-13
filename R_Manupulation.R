c1aa064fe995b1f60935ce0971e18cc680568105
Madhur Thakur <109874780+madhur006@users.noreply.github.com>
2025-11-13 11:21:28 -0500

    filter inside a filter

--- R_Manupulation.R
+++ R_Manupulation.R
@@ -4,10 +4,11 @@ getwd()
 # is there any duplicated 
 any(duplicated(donors_saf$DONOR_ID))
 
+
 # missing values 
 
-
 df %>%
+group_by(year) %>%
   summarise(
     across(
       .cols = c(col1, col2), 
@@ -36,6 +37,20 @@ write.xlsx(
   rowNames = FALSE 
 )
 
+
+### Filter inside a filter
+df %>%
+  filter(yr >= '2021') %>%
+  group_by(yr) %>%
+  summarise(
+    total_rows = n(),
+    total_distinct_ids = n_distinct(ID),
+    distinct_ids_where_dob_na = n_distinct(ID[!is.na(dob)),
+    distinct_ids_where_dob_na_state_NJNY = n_distinct(donor_id[!is.na(dob) & state %in% c('NY', 'NJ')]),
+    total_rows_where_dob_na_state_NJNY = sum(!is.na(dob)  & state %in% c('NY', 'NJ')),
+  )
+
+    
 # group by 
 # percent and number
 df %>%
